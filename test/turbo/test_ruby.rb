a6b95de89fa8d30a8a51a04e8f03ede73710a4ce
Marco Roth <marco.roth@intergga.ch>
2023-01-07 12:17:43 +0100

    remove failing test

--- test/turbo/test_ruby.rb
+++ test/turbo/test_ruby.rb
@@ -6,8 +6,4 @@ class Turbo::TestRuby < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Turbo::Ruby::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
