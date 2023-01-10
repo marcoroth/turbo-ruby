3e5e207a55da257ca90fce990040f180f8e3f175
Marco Roth <marco.roth@intergga.ch>
2023-01-10 12:16:59 +0100

    Import `Turbo::Elements::TurboFrame` element from `phlex`
    Joel removed the Turbo elements from Phlex in this pull request: 
    https://github.com/joeldrapper/phlex/pull/426
    
    We are just going to take over the basic element to have something to 
    start with.

--- lib/turbo/elements/turbo_frame.rb
+++ lib/turbo/elements/turbo_frame.rb
@@ -4,6 +4,25 @@ module Turbo
   module Elements
     class TurboFrame < Phlex::HTML
       register_element :turbo_frame
+
+      def initialize(src:, loading:, disabled:, target:, autoscroll:)
+        @src = src
+        @loading = loading
+        @disabled = disabled
+        @target = target
+        @autoscroll = autoscroll
+      end
+
+      def template(&content)
+        turbo_frame(
+          src: @src,
+          loading: @loading,
+          disabled: @disabled,
+          target: @target,
+          autoscroll: @autoscroll,
+          &content
+        )
+      end
     end
   end
 end
